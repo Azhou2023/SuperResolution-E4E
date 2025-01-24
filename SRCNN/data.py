42d411870be3e084c3787e986cf89d7eb844bab7
AndrewZhou2023 <57163201+Azhou2023@users.noreply.github.com>
2025-01-23 16:22:11 -0800

    training

--- SRCNN/data.py
+++ SRCNN/data.py
@@ -4,22 +4,28 @@ import torch
 
 class SRCNNDataset(torch.utils.data.Dataset):
     def __init__(self, hr_images, scale_factor):
-        self.hr_images = hr_images
-        self.scale_factor = scale_factor
+        hr = []
+        lr = []
+        for idx, hr_image in enumerate(hr_images):
+            hr_image = Image.fromarray(hr_image)
+            hr_image = hr_image.resize(((hr_image.width//scale_factor)*scale_factor, (hr_image.height//scale_factor)*scale_factor), resample=Image.BICUBIC)
+            lr_image = hr_image.resize((hr_image.width//scale_factor, hr_image.height//scale_factor), resample=Image.BICUBIC)  
+            lr_image = lr_image.resize((lr_image.width*scale_factor, lr_image.height*scale_factor), resample=Image.BICUBIC)
+
+            hr.append(hr_image)
+            lr.append(lr_image)
+        self.hr_images = np.transpose(hr, (0, 3, 1, 2))
+        self.lr_images = np.transpose(lr, (0, 3, 1, 2))
     def __len__(self):
         return len(self.hr_images)
     def __getitem__(self, index):
         hr_image = self.hr_images[index]
-        hr_image = Image.fromarray(hr_image)
-        hr_image = hr_image.resize(((hr_image.width//self.scale_factor)*self.scale_factor, (hr_image.height//self.scale_factor)*self.scale_factor), resample=Image.BICUBIC)
+        lr_image = self.lr_images[index]
 
-        lr_image = hr_image.resize((hr_image.width//self.scale_factor, hr_image.height//self.scale_factor), resample=Image.BICUBIC)  
-        lr_image = lr_image.resize((lr_image.width*self.scale_factor, lr_image.height*self.scale_factor), resample=Image.BICUBIC)
+        hr_image = torch.tensor(hr_image, dtype=torch.float32)
+        hr_image.div_(255)
 
-        hr_image = np.array(hr_image, dtype="float32")
-        hr_image/=255
+        lr_image = torch.tensor(lr_image, dtype=torch.float32)
+        lr_image.div_(255)
 
-        lr_image = np.array(lr_image, dtype="float32")
-        lr_image/=255
-        
-        return torch.tensor(lr_image.T), torch.tensor(hr_image.T)
+        return lr_image, hr_image
